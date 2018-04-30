cc3369aa7a89b28fd628153784c5406afdbbc5fd
Kiur Jullis <kiur.jullis@gmail.com>
2018-04-30 09:14:55 +0300

    player move

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
+
+
+const tileWidth = 101;
+const tileHeight = 83;
 // Enemies our player must avoid
-var Enemy = function (x, y, speed) {
+let Enemy = function (x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
+
     this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -13,6 +18,10 @@ var Enemy = function (x, y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
+   
+
+    this.x += (this.speed * dt);
+    if (this.x > 500) this.x = -100;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -21,12 +30,15 @@ Enemy.prototype.update = function (dt) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    if (this.x > 500) {
+        this.x -= 600;
+    }
 };
 
 // Now write your own player class
 let Player = function (x,y) {
         this.x = 202;
-        this.y = 380;
+        this.y = 390;
         this.sprite = 'images/char-boy.png';
 };
 
@@ -65,7 +77,7 @@ Player.prototype.handleInput = function (key) {
 // Place all enemy objects in an array called allEnemies
 const allEnemies = [];
 // Place the player object in a variable called player
-let player = new Player;
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
