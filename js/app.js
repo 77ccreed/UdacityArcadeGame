7d5fd43a9237b7f3576c9413d07e4b02a5a33a81
Kiur Jullis <kiur.jullis@gmail.com>
2018-05-03 12:43:36 +0300

    getRandomInt(3)

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ function randomSpeed() {
     return (Math.floor(Math.random() * 160) + 40);
 }
 
+function getRandomInt(max) {
+    return (Math.floor(Math.random() * Math.floor(max) + 1) * 63);
+}
+
 const tileWidth = 101;
 const tileHeight = 83;
 // Enemies our player must avoid
@@ -26,14 +30,13 @@ class Enemy {
     update(dt) {
         if (this.x < 505) {
             this.x += this.speed * dt;
-        }
-        else {
+        } else {
             this.x = -70;
         }
         //2D collision detection function from: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-        if (this.x < player.x + 25 &&
+        if (this.x < player.x + 35 &&
             this.x + 60 > player.x &&
-            this.y < player.y + 52 &&
+            this.y < player.y + 62 &&
             65 + this.y > player.y) {
             player.x = 202;
             player.y = 390;
@@ -50,22 +53,65 @@ class Enemy {
     }
 }
 
-
-
-// Now write your own player class
-let Player = function (x,y) {
+class Player {
+    constructor(y, x) {
         this.x = 202;
         this.y = 390;
         this.sprite = 'images/char-boy.png';
+    }
+    update(dt) {
+        if (this.y < 0 && this.x===202) {
+            this.x = 202;
+            this.y = 390;
+        }
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    handleInput(key) {
+        switch (key) {
+            case 'up':
+                if (this.y > 0) {
+                    this.y -= tileHeight;
+                }
+                break;
+            case 'down':
+                if (this.y < 370) {
+                    this.y += tileHeight;
+                }
+                break;
+            case 'left':
+                if (this.x >= tileWidth) {
+                    this.x -= tileWidth;
+                }
+                break;
+            case 'right':
+                if (this.x <= 303) {
+                    this.x += tileWidth;
+                }
+                break;
+
+        }
+    }
+
+}
+// Now write your own player class
+/*
+let Player = function (x, y) {
+    this.x = 202;
+    this.y = 390;
+    this.sprite = 'images/char-boy.png';
+
 };
 
 
-    Player.prototype.update = function (dt) {};
-    Player.prototype.render = function () {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
-    // This class requires an update(), render() and
-    // a handleInput() method.
+Player.prototype.update = function (dt) {};
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// This class requires an update(), render() and
+// a handleInput() method.
 Player.prototype.handleInput = function (key) {
     switch (key) {
         case 'up':
@@ -89,18 +135,23 @@ Player.prototype.handleInput = function (key) {
             }
             break;
     }
-};
+
+ console.log(this.x, this.y);
+ 
+};*/
+
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 
 
-const newEnemy = new Enemy(63, randomSpeed());
-const newEnemy1 = new Enemy(63, randomSpeed());
-const newEnemy2 = new Enemy(147, randomSpeed());
-const newEnemy3 = new Enemy(147, randomSpeed());
-const newEnemy4 = new Enemy(230, randomSpeed());
-const newEnemy5 = new Enemy(230, randomSpeed());
+const newEnemy = new Enemy(getRandomInt(3), randomSpeed());
+const newEnemy1 = new Enemy(getRandomInt(3), randomSpeed());
+const newEnemy2 = new Enemy(getRandomInt(3), randomSpeed());
+const newEnemy3 = new Enemy(getRandomInt(3), randomSpeed());
+const newEnemy4 = new Enemy(getRandomInt(3), randomSpeed());
+const newEnemy5 = new Enemy(getRandomInt(3), randomSpeed());
 
 const allEnemies = [newEnemy, newEnemy1, newEnemy2, newEnemy3, newEnemy4, newEnemy5];
 // Place the player object in a variable called player
